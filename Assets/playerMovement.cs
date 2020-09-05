c45712792e60ce6e22438de07f8f6b6daf12be95
dkingram823 <56693812+dkingram823@users.noreply.github.com>
2020-09-04 21:53:12 -0400

    Add files via upload

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -11,6 +11,9 @@ public class playerMovement : MonoBehaviour
         playerLoc = 0;
     }
 
+    public GameObject playerIdle;
+    public CharacterController contoller;
+
     // Update is called once per frame
     void Update()
     {
@@ -21,21 +24,43 @@ public class playerMovement : MonoBehaviour
         if (y > 0 && playerLoc>=2) //press up
         {
             playerLoc -= 2;
+            Debug.Log("up" + playerLoc);
         }else if (y < 0 && playerLoc<=1) //press down
         {
             playerLoc += 2;
+            Debug.Log("down" + playerLoc);
         }
         if (x > 0 && playerLoc%2==0) //press right
         {
             playerLoc += 1;
+            Debug.Log("right" + playerLoc);
         }
         else if (x < 0 && playerLoc%2==1) //press left
         {
             playerLoc -= 1;
+            Debug.Log("left" + playerLoc);
         }
 
+        int playertop = 1;
+        if (playerLoc < 2) //player is in up position
+            playertop = 0;
 
+        if (playerLoc % 2 == 0) //player is on left
+        {
+            playerIdle.transform.localPosition = new Vector3(1f, 2f - (3 * playertop), -3.5f);
+        }
+        else if (playerLoc % 2 == 1) //player is on right
+        {
+            playerIdle.transform.localPosition = new Vector3(1f, 2f - (3 * playertop), 0f);
+        }
 
-
+        if (playerLoc < 2) //player is on top
+        {
+            playerIdle.transform.localPosition = new Vector3(1f, 2f, -3.5f+(3.5f*(playerLoc % 2)));
+        }
+        else if (playerLoc >=2) //player is on bottom
+        {
+            playerIdle.transform.localPosition = new Vector3(1f, -1f, -3.5f + (3.5f * (playerLoc % 2)));
+        }
     }
 }
